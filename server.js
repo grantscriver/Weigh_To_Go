9017915175359b65ed732b0e7cc6019971fe17fb
reganmurray22 <67241915+reganmurray22@users.noreply.github.com>
2020-10-21 21:15:48 -0500

    backend routes, sequelize table and database

--- server.js
+++ server.js
@@ -1,48 +1,23 @@
-require("dotenv").config();
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-var db = require("./models");
+//connecting DB
+const db = require('./models');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+app.get("/", (req, res) => res.send("INDEX"));
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+//routes
+app.use("/app", require("./routes/app"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+const PORT = process.env.PORT || 3000;
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
   });
-});
+  
 
-module.exports = app;
